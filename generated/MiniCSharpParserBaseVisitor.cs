cf8490629063ebde26ea6772aeca26e36187f3bb
rooseveltalej <roperez@estudiantec.cr>
2025-06-02 10:28:06 -0600

    Update generated parser files to reflect new file paths

--- generated/MiniCSharpParserBaseVisitor.cs
+++ generated/MiniCSharpParserBaseVisitor.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-// Generated from C:/Users/Bayron/RiderProjects/compiladores/MiniCSharpParser.g4 by ANTLR 4.13.2
+// Generated from /Volumes/macOs/rooseveltalej/Documents/Compiladores/MiniCSharpParser.g4 by ANTLR 4.13.2
 
 // Unreachable code detected
 #pragma warning disable 0162
